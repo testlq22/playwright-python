5ce32f3a3bb9c1ecfa468fbea79509c0e73be851
testlq22 <ivanooleg119@gmail.com>
2023-03-23 16:26:59 +0200

    update test_data.py

--- utils/test_data.py
+++ utils/test_data.py
@@ -21,7 +21,7 @@ class Data:
     mobileNumber = '512-765-4321'
     subject = 'I need help'
     message = 'Help me to read this file'
-    fileToUpload = 'playwright-python/utils/test.txt'
+    fileToUpload = '/home/runner/work/playwright-python/playwright-python/utils/test.txt'
     product = 'Dress'
     someProduct = 'saree'
     product1Char = ['Blue Top', 'Rs. 500', '1', 'Rs. 500']
